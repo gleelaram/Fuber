2e43db9a742881375a185eb922371f190005f5ab
gleelaram <31830058+gleelaram@users.noreply.github.com>
2017-09-12 20:35:02 +0530

    updating the service

--- fuber1/src/main/java/com/quintype/fuber1/service/CabService.java
+++ fuber1/src/main/java/com/quintype/fuber1/service/CabService.java
@@ -3,10 +3,11 @@ package com.quintype.fuber1.service;
 import java.util.Set;
 
 import com.quintype.fuber1.dto.Car;
+import com.quintype.fuber1.dto.RideDtls;
 import com.quintype.fuber1.dto.User;
 
 public interface CabService {
 	
-public Car getNearestCar(User user);
+public RideDtls getNearestCar(User user);
 public Set<Car> getAllCars();
 }
